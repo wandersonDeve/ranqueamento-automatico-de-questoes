ecc6e8e8fc24c19258fd132c0afb92fc397e62e3
Wanderson Santos <wanderlavo@gmail.com>
2022-02-07 15:06:10 -0300

    entity atualizada com os novas campos da model pergunta

--- src/cadastrar-perguntas/entities/cadastrar-pergunta.entity.ts
+++ src/cadastrar-perguntas/entities/cadastrar-pergunta.entity.ts
@@ -4,9 +4,13 @@ export class CadastrarPergunta implements Prisma.PerguntaUncheckedCreateInput{
     id?: number;
     pergunta: string;
     rank?: string;
+    letra_a: string;
+    letra_b: string;
+    letra_c: string;
+    letra_d: string;
+    letra_e: string;
     resposta_certa: string;
-    qtn_respotas_certas: number;
-    qtn_respotas_erradas: number;
-    opcoes?: Prisma.OpcoesUncheckedCreateNestedOneWithoutPerguntaInput;
-   
+    qtn_respotas_certas?: number;
+    qtn_respotas_erradas?: number;
+
 }
